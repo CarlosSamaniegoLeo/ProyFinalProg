7b3fbe558b32bcb640eb51ea5b2bac20e0199e25
CarlosSamaniegoLeo <carlossama10@gmail.com>
2023-05-11 10:20:56 +0200

    Comienzo de proyecto

---
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.proyectofinal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    void Logear(ActionEvent event) {
+
+    }
+
+}
