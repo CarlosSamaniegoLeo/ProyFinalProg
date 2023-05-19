1eb079d437558b583ed9fd5e55d5fa951e0ffb70
CarlosSamaniegoLeo <carlossama10@gmail.com>
2023-05-19 10:04:03 +0200

    a ver ahora

---
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/InicioController.java
@@ -0,0 +1,61 @@
+package com.example.proyectofinal;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class InicioController {
+
+    @FXML
+    private Button btnBusqueda;
+
+    @FXML
+    private Button btnFavoritos;
+
+    @FXML
+    private Button btnNoticia;
+
+    @FXML
+    private Button btnnoticia2;
+
+    @FXML
+    private TextField txtBusqueda;
+
+    @FXML
+    private Button txtCalendario;
+
+    @FXML
+    private Button txtClasificacion;
+
+    @FXML
+    void Buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irCalendario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irClasificacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irFavoritos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irNoticia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irnoticia2(ActionEvent event) {
+
+    }
+
+}
