66be9f43021bd33d8f872fa376d7debde476d8e6
CarlosSamaniegoLeo <carlossama10@gmail.com>
2023-05-19 10:45:14 +0200

    YA ESTA

--- ProyectoFinal/src/main/java/module-info.java
+++ ProyectoFinal/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.proyectofinal {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens com.example.proyectofinal to javafx.fxml;
     exports com.example.proyectofinal;
