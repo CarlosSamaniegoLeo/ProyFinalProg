4b9ab6631ced93b1167d41c045519745dba4e150
CarlosSamaniegoLeo <carlossama10@gmail.com>
2023-05-22 11:55:46 +0200

    Nuevos Cambios

--- ProyectoFinal/src/main/java/com/example/proyectofinal/LoginController.java
+++ ProyectoFinal/src/main/java/com/example/proyectofinal/LoginController.java
@@ -28,16 +28,8 @@ public class LoginController {
     @FXML
     private TextField txtUser;
 
-    // Crear un HashMap y agregar algunos datos
-    Map<String, String> mapa= new HashMap<>();
+    public void initialize() {
 
-    void initialize() {
-        // Añadir valores iniciales al HashMap
-        mapa.put("Carlos123", "Carlos");
-        mapa.put("Sergio123", "Sergio");
-        mapa.put("Victor123", "Victor");
-        mapa.put("Ivan123", "Ivan");
-        mapa.put("Alvaro123", "Alvaro");
     }
     @FXML
     void Logear(ActionEvent event) {
